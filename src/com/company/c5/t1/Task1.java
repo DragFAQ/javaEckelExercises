ae7a00b747866d305236e08183d2e8a125bf1d60
Drag <s.safonov@bunet.biz>
2016-10-09 15:46:01 +0300

    test

--- src/com/company/c5/t1/Task1.java
+++ src/com/company/c5/t1/Task1.java
@@ -3,7 +3,7 @@ package com.company.c5.t1;
 import static net.mindview.util.Print.print;
 
 /**
- * Created by drag on 09.09.2016.
+ * Created by drag on 09.09.2016
  */
 public class Task1 {
     public static void main(String[] args) {
