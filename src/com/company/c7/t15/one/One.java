f3bc18b66a214c88d76bafd9938f3ff6e1c98b46
Drag <s.safonov@bunet.biz>
2016-10-11 23:47:47 +0300

    Chapter 7 / Task 15

---
+++ src/com/company/c7/t15/one/One.java
@@ -0,0 +1,12 @@
+package com.company.c7.t15.one;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by Drag on 11.10.2016.
+ */
+public class One {
+    protected void some() {
+        print("One some");
+    }
+}
---
+++ src/com/company/c7/t15/two/Two.java
@@ -0,0 +1,16 @@
+package com.company.c7.t15.two;
+
+import com.company.c7.t15.one.One;
+
+/**
+ * Created by Drag on 11.10.2016.
+ */
+public class Two extends One{
+    public static void main(String[] args) {
+        One o = new One();
+        //o.some();
+
+        Two t = new Two();
+        t.some();
+    }
+}
